f69f19f0cc4943209886b052d9e19c2eaa8e6640
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2017-06-25 18:06:07 +0200

    Blocking sendMessage function in messenger-notifier

--- messenger-notifier.js
+++ messenger-notifier.js
@@ -16,18 +16,28 @@ async function notifyUsersAboutOffers(offersIds) {
     for (let { mid } of userIds) {
         for (let offer of offers) {
             console.log(`...sending offer ${offer.id} to ${mid}`);
-            bot.sendMessage(Number(mid), {
-                text: `New offer!\nTitle: ${offer.title}\nRooms: ${offer.rooms}\nSize: ${offer.size}\nLink: ${offer.link},\nRent: ${offer.rent}`,
-                attachment: {
-                    type: 'image',
-                    payload: offer.image_url
-                }
-            }, (err, info) => {
-                console.log('err', err)
-                console.log('info', info)
-            })
+            await sendMessage(mid, offer)
         }
     }
+}
+
+function sendMessage(mid, offer) {
+    return new Promise((resolve, reject) => {
+        bot.sendMessage(Number(mid), {
+            text: `New offer!\nTitle: ${offer.title}\nRooms: ${offer.rooms}\nSize: ${offer.size}\nLink: ${offer.link},\nRent: ${offer.rent}`,
+            attachment: {
+                type: 'image',
+                payload: offer.image_url
+            }
+        }, (err, info) => {
+            if (err) {
+                console.log(err)
+                return reject(err)
+            }
+            console.log(info)
+            resolve(info)
+        })
+    })
 }
 
 module.exports = {
