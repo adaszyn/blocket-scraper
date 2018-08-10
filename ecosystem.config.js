6d9cfaa29d2d656e80aa9a9ab5cb01825c96f9a2
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2018-08-10 21:44:54 +0200

    ecosystem.config added

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [
+        {
+            name: 'BLOCKET-SCRAPER',
+            script: 'cron-service.js',
+        }
+    ],
+    deploy: {
+        production: {
+            user: 'wojtek',
+            host: process.env.EC2_INSTANCE_IP,
+            ref: 'origin/master',
+            repo: 'git@github.com:wojciechAdaszynski/blocket-scraper.git',
+            path: '/opt/blocket-scraper',
+            'post-deploy': 'NODE_ENV=production npm install && npm run migrate && pm2 reload ecosystem.config.js --env production'
+        }
+    }
+};
